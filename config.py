61a6b5c545cabd224dbc1c6f58c6b6ebf01d1ba2
Spencer Moon <spencer.s.moon@gmail.com>
2018-03-06 13:50:10 -0600

    Add config

---
+++ config.py
@@ -0,0 +1,11 @@
+# Local database
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///../../data/data.sqlite'
+#LOCAL_PATH = '../data/data.sqlite'
+DB_PATH = '../../data/database.sqlite'
+
+# RDS database
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://spencer:msia_423@db.c70edrasw7bt.us-west-2.rds.amazonaws.com:3306/epl'
+
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'sbQm38a1awYXbmfWLgZaHCupa8egevTiZusvHx/G'
