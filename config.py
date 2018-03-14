6034670a344edcdaa8c69de79ce69fd21c648493
Spencer Moon <spencer.s.moon@gmail.com>
2018-03-13 21:27:54 -0500

    Update config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 DB_PATH = '../../data/database.sqlite'
 
 # RDS database
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://spencer:msia_423@db.c70edrasw7bt.us-west-2.rds.amazonaws.com:3306/epl'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://spencer:msia_423@soccer.c70edrasw7bt.us-west-2.rds.amazonaws.com:3306/epl'
 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
